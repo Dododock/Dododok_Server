8d8ad850889b7db5db26a3471c816fc75b17e0a7
bo <jacobhboy66@gmail.com>
2023-12-27 15:27:42 +0900

    chore(root): 프로젝트 초기화

---
+++ src/main/java/neo/nae/dododok/DododokApplication.java
@@ -0,0 +1,13 @@
+package neo.nae.dododok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DododokApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DododokApplication.class, args);
+	}
+
+}
---
+++ src/test/java/neo/nae/dododok/DododokApplicationTests.java
@@ -0,0 +1,13 @@
+package neo.nae.dododok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DododokApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
