2c71a9486c3eba9b0ace54c62acdf459c3a4bb8b
bo <jacobhboy66@gmail.com>
2023-12-27 23:55:56 +0900

    chore(Application): @EnableJpaRepository 어노테이션 추가

--- src/main/java/neo/nae/dododok/DododokApplication.java
+++ src/main/java/neo/nae/dododok/DododokApplication.java
@@ -2,8 +2,10 @@ package neo.nae.dododok;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories
 public class DododokApplication {
 
 	public static void main(String[] args) {
